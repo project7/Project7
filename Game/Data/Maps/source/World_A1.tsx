9dd2e512d1efb1a0a2a8f72e8f47666e5302becd
Yangff <Yangff1@gmail.com>
2012-06-20 21:12:52 +0800

    Signed-off-by: Yangff <Yangff1@gmail.com> * 提出新的地图格式（用这个还是用VA的请尽快决定！！反正最后都是png。。） * 完善game.exe的兼容性

---
+++ Game/Data/Maps/source/World_A1.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="World_A1" tilewidth="16" tileheight="16">
+ <properties>
+  <property name="movement" value="1"/>
+  <property name="pass" value="0"/>
+ </properties>
+ <image source="../../../Graphics/Tilesets/World_A1.png" width="512" height="384"/>
+</tileset>
---
+++ Game/Data/Maps/source/World_A2.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="World_A2" tilewidth="16" tileheight="16">
+ <properties>
+  <property name="pass" value="1"/>
+ </properties>
+ <image source="../../../Graphics/Tilesets/World_A2.png" width="512" height="384"/>
+ <tile id="481">
+  <properties>
+   <property name="pass" value="1"/>
+  </properties>
+ </tile>
+</tileset>
