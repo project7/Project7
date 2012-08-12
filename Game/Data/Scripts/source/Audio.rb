003876abf95b628903294992071b82c0aebb0c0b
Yeechan Lu <wz.bluesnow@gmail.com>
2012-08-12 18:57:17 +0800

    Volume Adjust
    Signed-off-by: Yeechan Lu <wz.bluesnow@gmail.com>

---
+++ Game/Data/Scripts/source/Audio.rb
@@ -0,0 +1,264 @@
+﻿#==============================================================================
+# ** Audio
+#------------------------------------------------------------------------------
+#  The module that carries out music and sound processing.
+#==============================================================================
+
+module Audio
+  #--------------------------------------------------------------------------
+  # * Constants
+  #--------------------------------------------------------------------------
+  BGM_FADE_IN_INCREMENT = 5     # BGM volume incremented 0.2 seconds
+  #--------------------------------------------------------------------------
+  # * Instance Variables
+  #--------------------------------------------------------------------------
+  @bgm_fading_out = false       # BGM started fading out
+  @bgm_fade_decrement = 0.0     # BGM volume decremented each update
+  @bgs_fading_out = false       # BGS started fading out
+  @bgs_fade_decrement = 0.0     # BGS volume decremented each update
+  @me_fading_out = false        # ME started fading out
+  @me_fade_decrement = 0.0      # ME volume decremented each update
+  @me_playing = false           # Is some ME playing?
+  @playing_bgm = nil            # BGM currently being played
+  @next_bgm = nil               # The BGM to be played after fading out
+  @next_bgm_position = 0        # Starting position of next bgm
+  @next_bgs = nil               # The BGS to be played after fading out
+  @next_bgs_position = 0        # Starting position of next bgm
+  @next_me = nil                # The ME to be played after fading
+  #--------------------------------------------------------------------------
+  # * Starts BGM Playback
+  #     name            : Name of the file
+  #     volume          : Channel volume
+  #     pitch           : Channel frequency
+  #     position        : Starting position in milliseconds
+  #-------------------------------------------------------------------------- 
+  def Audio.bgm_play(filename, volume = 100, pitch = 100, position = 0, fade_in = false)
+    if @bgm_fading_out and !fade_in
+      @next_bgm = RPG::AudioFile.new(filename, volume, pitch)
+      @next_bgm_position = position
+      return
+    end
+    start_volume = volume
+    if fade_in
+      @bgm_target_volume = volume unless @bgm_fading_in
+      @bgm_fading_in = true
+      start_volume = 0
+    end
+    @bgm_fading_out = false
+    # If a ME is playing we wait until it's over before playing BGM
+    unless @me_playing
+      FMod::bgm_play(filename, start_volume, pitch, position)
+    end
+    @playing_bgm = RPG::AudioFile.new(filename, volume, pitch)
+    @memorized_bgm = @playing_bgm
+    @memorized_bgm_position = position
+  end
+  #--------------------------------------------------------------------------
+  # * Stops BGM Playback
+  #-------------------------------------------------------------------------- 
+  def Audio.bgm_stop
+    @memorized_bgm = nil
+    @playing_bgm = nil
+    @bgm_fading_in = false
+    @bgm_fading_out = false
+    # MEs are internally BGMs, but are stopped with me_stop instead
+    if @me_playing
+      return
+    end
+    FMod::bgm_stop
+  end
+  def Audio.bgm_pos
+    FMod::bgm_position
+  end
+  def Audio.bgs_pos
+    FMod::bgs_position
+  end
+  #--------------------------------------------------------------------------
+  # * Starts BGM fadeout.
+  #     time            : Length of the fadeout in milliseconds.
+  #-------------------------------------------------------------------------- 
+  def Audio.bgm_fade(time)
+    return if @me_playing or !FMod::bgm_playing?
+    @bgm_fading_out = true
+    time = time / 1000
+    @bgm_fade_decrement = FMod::bgm_volume / (time * 5)
+  end
+  #--------------------------------------------------------------------------
+  # * Starts BGS Playback
+  #     name            : Name of the file
+  #     volume          : Channel volume
+  #     pitch           : Channel frequency
+  #     position        : Starting position in milliseconds
+  #-------------------------------------------------------------------------- 
+  def Audio.bgs_play(filename, volume = 100, pitch = 100, position = 0)
+    if @bgs_fading_out
+      @next_bgs = RPG::AudioFile.new(filename, volume, pitch)
+      @next_bgs_position = position
+      return
+    end
+    FMod::bgs_play(filename, volume, pitch, position)
+  end
+  #--------------------------------------------------------------------------
+  # * Stops BGS Playback
+  #-------------------------------------------------------------------------- 
+  def Audio.bgs_stop
+    FMod::bgs_stop
+    @bgs_fading_out = false
+  end
+  #--------------------------------------------------------------------------
+  # * Starts BGS fadeout.
+  #     time            : Length of the fadeout in milliseconds.
+  #-------------------------------------------------------------------------- 
+  def Audio.bgs_fade(time)
+    return unless FMod::bgs_playing?
+    @bgs_fading_out = true
+    time = time / 1000
+    @bgs_fade_decrement = FMod::bgs_volume / (time * 5)
+  end
+  #--------------------------------------------------------------------------
+  # * Starts ME Playback
+  #     name            : Name of the file
+  #     volume          : Channel volume
+  #     pitch           : Channel frequency
+  #-------------------------------------------------------------------------- 
+  def Audio.me_play(filename, volume = 100, pitch = 100)
+    if @me_fading_out
+      @next_me = RPG::AudioFile.new(filename, volume, pitch)
+      return
+    end
+    if @bgm_fading_out
+      self.bgm_stop
+    end
+    # Memorize playing bgm
+    if @playing_bgm and !@me_playing
+      bgm = @playing_bgm
+      @playing_bgm = RPG::AudioFile.new(bgm.name, FMod::bgm_volume, bgm.pitch)
+      @memorized_bgm = @playing_bgm
+      @memorized_bgm_position = FMod::bgm_position
+    end
+    @me_playing = true
+    FMod::bgm_play(filename, volume, pitch, 0, false)
+  end
+  #--------------------------------------------------------------------------
+  # * Stops ME Playback
+  #-------------------------------------------------------------------------- 
+  def Audio.me_stop
+    return unless @me_playing
+    @me_playing = false
+    @me_fading_out = false
+    # Play memorized bgm, fading in
+    if @memorized_bgm and !@bgm_fading_out
+      bgm = @memorized_bgm
+      self.bgm_play(bgm.name, bgm.volume, bgm.pitch, @memorized_bgm_position, true)
+    else
+      self.bgm_stop
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Starts ME fadeout.
+  #     time            : Length of the fadeout in milliseconds.
+  #-------------------------------------------------------------------------- 
+  def Audio.me_fade(time)
+    return unless FMod::bgm_playing?
+    @me_fading_out = true
+    time = time / 1000
+    @bgm_fade_decrement = FMod::bgm_volume / (time * 5)
+  end
+  #--------------------------------------------------------------------------
+  # * Starts SE Playback
+  #     name            : Name of the file
+  #     volume          : Channel volume
+  #     pitch           : Channel frequency
+  #-------------------------------------------------------------------------- 
+  def Audio.se_play(filename, volume = 100, pitch = 100) 
+    FMod::se_play(filename, volume, pitch)
+  end
+  #--------------------------------------------------------------------------
+  # * Stops SE Playback
+  #-------------------------------------------------------------------------- 
+  def Audio.se_stop 
+    FMod::se_stop
+  end
+  #--------------------------------------------------------------------------
+  # * Update ME Playback, SE Disposal and Fading, Called Each Frame
+  #-------------------------------------------------------------------------- 
+  def Audio.update
+    # Stop ME when it's over (and continue playing BGM)
+    if @me_playing
+      unless FMod::bgm_playing?
+        self.me_stop
+      end
+    end
+    # Remove any finished SEs
+    unless FMod::se_list_empty?
+      FMod::se_clean
+    end
+    if @bgm_fading_in
+      # Stop fading when target is reached, otherwise increase volume
+      if FMod::bgm_volume >= @bgm_target_volume
+        @bgm_fading_in = false
+      else
+        current_volume = FMod::bgm_volume + BGM_FADE_IN_INCREMENT
+        FMod::bgm_volume = current_volume
+      end
+    end
+    if FMod::bgm_playing? and @bgm_fading_out and 
+        !@me_playing
+      if FMod::bgm_volume <= 0
+        @bgm_fading_out = false
+        self.bgm_stop
+        # If another BGM played while fading out, play it (most recent)
+        if @next_bgm
+          self.bgm_play(@next_bgm.name, @next_bgm.volume,
+                        @next_bgm.pitch, @next_bgm_position)
+          @next_bgm = nil
+        end
+      else
+        current_volume = FMod::bgm_volume - @bgm_fade_decrement
+        FMod::bgm_volume = current_volume
+      end
+    end
+    if FMod::bgs_playing? and @bgs_fading_out
+      if FMod::bgs_volume <= 0
+        @bgs_fading_out = false
+        self.bgs_stop
+        # If another BGS played while fading out, play it (most recent)
+        if @next_bgs
+          self.bgs_play(@next_bgs.name, @next_bgs.volume, 
+                        @next_bgs.pitch, @next_bgs_position)
+          @next_bgs = nil
+        end
+      else
+        current_volume = FMod::bgs_volume - @bgs_fade_decrement
+        FMod::bgs_volume = current_volume
+      end
+    end
+    if FMod::bgm_playing? and @me_fading_out
+      if FMod::bgm_volume <= 0
+        # If another ME played while fading out, play it (most recent)
+        if @next_me
+          self.me_play(@next_me.name, @next_me.volume, @next_me.pitch)
+          @next_me = nil
+        else
+          @me_fading_out = false
+          self.me_stop
+        end
+      else
+        current_volume = FMod::bgm_volume - @bgm_fade_decrement
+        FMod::bgm_volume = current_volume
+      end
+    end
+  end
+  
+  def self.sync_vol
+    FMod.sync_vol
+  end
+end
+
+# Create an endless loop to update Audio module
+Thread.new do
+  loop do
+    sleep 0.2
+    Audio.update
+  end
+end
---
+++ Game/Data/Scripts/source/FMod.rb
@@ -0,0 +1,399 @@
+﻿#==============================================================================
+# ** FMod
+#------------------------------------------------------------------------------
+#  A higher level module to access FMOD Ex
+#==============================================================================
+
+module FMod
+  
+  #============================================================================
+  # ** SoundFile
+  #----------------------------------------------------------------------------
+  #  Represents a Sound file (BGM, BGS, SE, etc.) and associated Channel
+  #============================================================================
+  
+  class SoundFile
+    #--------------------------------------------------------------------------
+    # * Public Instance Variables
+    #--------------------------------------------------------------------------
+    attr_accessor :name                     # File name
+    attr_accessor :sound                    # FModEx::Sound object
+    attr_accessor :channel                  # Channel playing sound
+    attr_accessor :volume                   # Volume in RPG::AudioFile format
+    attr_accessor :pitch                    # Pitch in RPG::AudioFile format
+    attr_accessor :looping                  # Sound loops
+    attr_accessor :streaming                # Sound is streamed
+    attr_accessor :length                   # Sound length in milliseconds
+    attr_accessor :base_volume              # Base volume
+    #--------------------------------------------------------------------------
+    # * Object Initialization
+    #--------------------------------------------------------------------------
+    def initialize(name, sound, channel, volume, pitch, looping, streaming, length)
+      @name = name
+      @sound = sound
+      @base_volume = 100.0
+      @channel = channel
+      @volume = volume
+      @pitch = pitch
+      @looping = looping
+      @streaming = streaming
+      @length = length
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Instance Variables
+  #--------------------------------------------------------------------------
+  @fmod_dll = FModEx::DLL.new               # The FMOD Ex DLL
+  @fmod = FModEx::System.new(@fmod_dll)     # The global System object
+  @fmod_se = []                             # Array of Sound Effects
+  @rtp_folder = ""                          # Name of RTP folder
+  #--------------------------------------------------------------------------
+  # * Return Proper File Name (With Extensions)
+  #     name            : Name of the file
+  #     extensions      : Extensions to add to file name
+  #-------------------------------------------------------------------------- 
+  def self.checkExtensions(name, extensions)
+    if FileTest.exist?(name)
+      return name
+    end
+    # Add extension if needed
+    extensions.each do |ext|
+      if FileTest.exist?(name + '.' + ext)
+        return name + '.' + ext
+      end
+    end
+    # File doesn't exist
+    return name
+  end
+  #--------------------------------------------------------------------------
+  # * Get Valid File Name
+  #     name            : Name of the file
+  #-------------------------------------------------------------------------- 
+  def self.selectBGMFilename(name)
+    name = name.gsub("/", "\\")
+    # See if file exists in game folder
+    localname = self.checkExtensions(name, FModEx::FMOD_FILE_TYPES)
+    if FileTest.exist?(localname)
+      return localname
+    end
+    # An invalid name was provided
+    return name
+  end
+  #--------------------------------------------------------------------------
+  # * Play a Sound File Then Return it
+  #     name            : Name of the file
+  #     volume          : Channel volume
+  #     pitch           : Channel frequency
+  #     position        : Starting position in milliseconds
+  #     looping         : Does the sound loop?
+  #     streaming       : Stream sound or load whole thing to memory?
+  #-------------------------------------------------------------------------- 
+  def self.play(name, volume, pitch, position, looping, streaming, base_volume = 100.0)
+    # Get a valid file name
+    filename = self.selectBGMFilename(name)
+    # Create Sound or Stream and set initial values
+    sound = streaming ? @fmod.createStream(filename) : @fmod.createSound(filename)
+    sound.loopMode = looping ? FModEx::FMOD_LOOP_NORMAL : FModEx::FMOD_LOOP_OFF
+    channel = sound.play
+    volume = volume * 1.0
+    pitch = pitch * 1.0
+    file_length = sound.length(FModEx::FMOD_DEFAULT_UNIT)
+    sound_file = SoundFile.new(filename, sound, channel, volume, 
+                                pitch, looping, streaming, file_length)
+    sound_file.base_volume = base_volume
+    sound_file.channel.volume = volume * base_volume / 10000.0
+    sound_file.channel.frequency = sound_file.channel.frequency * pitch / 100
+    sound_file.channel.position = position
+    return sound_file
+  end
+  #--------------------------------------------------------------------------
+  # * Stop and Dispose of Sound File
+  #-------------------------------------------------------------------------- 
+  def self.stop(sound_file)
+    unless sound_file and sound_file.channel
+      return
+    end
+    # Stop channel, then clear variables and dispose of bgm
+    sound_file.channel.stop
+    sound_file.channel = nil
+    sound_file.sound.dispose
+  end
+  #--------------------------------------------------------------------------
+  # * Return Length in Milliseconds
+  #-------------------------------------------------------------------------- 
+  def self.get_length(sound_file, unit = FModEx::FMOD_DEFAULT_UNIT)
+    return sound_file.length(unit)
+  end
+  #--------------------------------------------------------------------------
+  # * Check if Another Sound File is Playing
+  #-------------------------------------------------------------------------- 
+  def self.already_playing?(sound_file, name, position = 0)
+    # Get a valid file name
+    filename = self.selectBGMFilename(name)
+    if (sound_file)
+      # If the same sound file is already playing don't play it again
+      if (sound_file.name == filename and position == 0)
+        return true
+      end
+      # If another sound file is playing, stop it
+      if sound_file.channel
+        self.stop(sound_file)
+      end
+    end
+    # No sound file is playing or it was already stopped
+    return false
+  end
+  #--------------------------------------------------------------------------
+  # * Check if Sound File is Playing
+  #--------------------------------------------------------------------------  
+  def self.playing?(sound_file)
+    unless sound_file and sound_file.channel
+      return false
+    end
+    return sound_file.channel.playing?
+  end
+  #--------------------------------------------------------------------------
+  # * Get Current Sound File Playing Position
+  #-------------------------------------------------------------------------- 
+  def self.get_position(sound_file)
+    unless sound_file and sound_file.channel
+      return 0
+    end
+    return sound_file.channel.position
+  end
+  #--------------------------------------------------------------------------
+  # * Seek to a New Sound File Playing Position
+  #-------------------------------------------------------------------------- 
+  def self.set_position(sound_file, new_pos)
+    unless sound_file and sound_file.channel
+      return
+    end
+    sound_file.channel.position = new_pos
+  end
+  #--------------------------------------------------------------------------
+  # * Get Current Sound File Volume
+  #-------------------------------------------------------------------------- 
+  def self.get_volume(sound_file)
+    unless sound_file
+      return 0
+    end
+    return sound_file.volume
+  end
+  #--------------------------------------------------------------------------
+  # * Set Sound File Volume
+  #-------------------------------------------------------------------------- 
+  def self.set_volume(sound_file, volume)
+    unless sound_file and sound_file.channel
+      return
+    end
+    sound_file.volume = volume * 1.0
+    sound_file.channel.volume = sound_file.base_volume * volume / 10000.0
+  end
+  #--------------------------------------------------------------------------
+  # * Set Loop Points
+  #     first           : Loop start point in milliseconds
+  #     second          : Loop end point in milliseconds (-1 for file end)
+  #     unit            : FMOD_TIMEUNIT for points
+  #-------------------------------------------------------------------------- 
+  def self.set_loop_points(sound_file, first, second, unit = FModEx::FMOD_DEFAULT_UNIT)
+    unless sound_file and sound_file.channel
+      return
+    end
+    # If second is -1 then set loop end to the file end
+    if second == -1
+      second = sound_file.length - 1
+    end
+    # Set loop points and reflush stream buffer
+    sound_file.channel.sound.setLoopPoints(first, second, unit)
+    sound_file.channel.position = sound_file.channel.position
+    return sound_file
+  end
+  #--------------------------------------------------------------------------
+  # * Play BGM (or ME)
+  #     name            : Name of the file
+  #     volume          : Channel volume
+  #     pitch           : Channel frequency
+  #     position        : Starting position in milliseconds
+  #     looping         : Does the BGM loop?
+  #-------------------------------------------------------------------------- 
+  def self.bgm_play(name, volume, pitch, position = 0, looping = true)
+    return if self.already_playing?(@fmod_bgm, name, position)
+    # Now play the new BGM as a stream
+    @fmod_bgm = self.play(name, volume, pitch, position, looping, true)
+  end
+  #--------------------------------------------------------------------------
+  # * Stop and Dispose of BGM
+  #-------------------------------------------------------------------------- 
+  def self.bgm_stop
+    self.stop(@fmod_bgm)
+    @fmod_bgm = nil
+  end
+  #--------------------------------------------------------------------------
+  # * Return BGM Length in Milliseconds
+  #-------------------------------------------------------------------------- 
+  def self.bgm_length(sound_file)
+    self.get_length(@fmod_bgm)
+  end
+  #--------------------------------------------------------------------------
+  # * Check if a BGM is Playing
+  #--------------------------------------------------------------------------  
+  def self.bgm_playing?
+    return self.playing?(@fmod_bgm)
+  end
+  #--------------------------------------------------------------------------
+  # * Get Current BGM Playing Position
+  #-------------------------------------------------------------------------- 
+  def self.bgm_position
+    return self.get_position(@fmod_bgm)
+  end
+  #--------------------------------------------------------------------------
+  # * Seek to New BGM Playing Position
+  #-------------------------------------------------------------------------- 
+  def self.bgm_position=(new_pos)
+    self.set_position(@fmod_bgm, new_pos)
+  end
+  #--------------------------------------------------------------------------
+  # * Get Current BGM Volume
+  #-------------------------------------------------------------------------- 
+  def self.bgm_volume
+    return self.get_volume(@fmod_bgm)
+  end
+  #--------------------------------------------------------------------------
+  # * Set BGM Volume
+  #-------------------------------------------------------------------------- 
+  def self.bgm_volume=(volume)
+    self.set_volume(@fmod_bgm, volume)
+  end
+  #--------------------------------------------------------------------------
+  # * Set Loop Points
+  #     first           : Loop start point in milliseconds
+  #     second          : Loop end point in milliseconds
+  #     unit            : FMOD_TIMEUNIT for points
+  #-------------------------------------------------------------------------- 
+  def self.bgm_set_loop_points(first, second, unit = FModEx::FMOD_DEFAULT_UNIT)
+    @fmod_bgm = self.set_loop_points(@fmod_bgm, first, second, unit)
+  end
+  #--------------------------------------------------------------------------
+  # * Play BGS
+  #     name            : Name of the file
+  #     volume          : Channel volume
+  #     pitch           : Channel frequency
+  #     position        : Starting position in milliseconds
+  #     looping         : Does the BGS loop?
+  #-------------------------------------------------------------------------- 
+  def self.bgs_play(name, volume, pitch, position = 0, looping = true)
+    return if self.already_playing?(@fmod_bgs, name, position)
+    # Now play the new BGS as a stream
+    @fmod_bgs = self.play(name, volume, pitch, position, looping, true, $syseting[:bgm_value])
+  end
+  #--------------------------------------------------------------------------
+  # * Stop and Dispose of BGS
+  #-------------------------------------------------------------------------- 
+  def self.bgs_stop
+    self.stop(@fmod_bgs)
+    @fmod_bgs = nil
+  end
+  #--------------------------------------------------------------------------
+  # * Return BGS Length in Milliseconds
+  #-------------------------------------------------------------------------- 
+  def self.bgm_length(sound_file)
+    self.get_length(@fmod_bgs)
+  end
+  #--------------------------------------------------------------------------
+  # * Check if a BGS is Playing
+  #--------------------------------------------------------------------------  
+  def self.bgs_playing?
+    return self.playing?(@fmod_bgs)
+  end
+  #--------------------------------------------------------------------------
+  # * Get Current BGS Playing Position
+  #-------------------------------------------------------------------------- 
+  def self.bgs_position
+    return self.get_position(@fmod_bgs)
+  end
+  #--------------------------------------------------------------------------
+  # * Seek to New BGS Playing Position
+  #-------------------------------------------------------------------------- 
+  def self.bgs_position=(new_pos)
+    self.set_position(@fmod_bgs, new_pos)
+  end
+  #--------------------------------------------------------------------------
+  # * Get Current BGS Volume
+  #-------------------------------------------------------------------------- 
+  def self.bgs_volume
+    return self.get_volume(@fmod_bgs)
+  end
+  #--------------------------------------------------------------------------
+  # * Set BGS Volume
+  #-------------------------------------------------------------------------- 
+  def self.bgs_volume=(volume)
+    self.set_volume(@fmod_bgs, volume)
+  end
+  #--------------------------------------------------------------------------
+  # * Set Loop Points
+  #     first           : Loop start point in milliseconds
+  #     second          : Loop end point in milliseconds
+  #     unit            : FMOD_TIMEUNIT for points
+  #-------------------------------------------------------------------------- 
+  def self.bgs_set_loop_points(first, second, unit = FModEx::FMOD_DEFAULT_UNIT)
+    @fmod_bgs = self.set_loop_points(@fmod_bgs, first, second, unit)
+  end
+  #--------------------------------------------------------------------------
+  # * Play SE
+  #     name            : Name of the file
+  #     volume          : Channel volume
+  #     pitch           : Channel frequency
+  #-------------------------------------------------------------------------- 
+  def self.se_play(name, volume, pitch, position = 0)
+    if @fmod_se.size > @fmod.maxChannels
+      se = @fmod_se.shift
+      self.stop(se)
+    end
+    # Load SE into memory and play it
+    @fmod_se << self.play(name, volume, pitch, position, false, false, $syseting[:se_value])
+  end
+  #--------------------------------------------------------------------------
+  # * Stop and Dispose of all SEs
+  #-------------------------------------------------------------------------- 
+  def self.se_stop
+    for se in @fmod_se
+      self.stop(se)
+    end
+    @fmod_se.clear
+  end
+  #--------------------------------------------------------------------------
+  # * Get Rid of Non-Playing SEs
+  #--------------------------------------------------------------------------  
+  def self.se_clean
+    for se in @fmod_se
+      unless self.playing?(se)
+        self.stop(se)
+        @fmod_se.delete(se)
+      end
+    end
+  end
+  #--------------------------------------------------------------------------
+  # * Check if There's Some SE in SE Array
+  #--------------------------------------------------------------------------  
+  def self.se_list_empty?
+    return @fmod_se.empty?
+  end
+  #--------------------------------------------------------------------------
+  # * Dispose of Everything
+  #--------------------------------------------------------------------------  
+  def self.dispose
+    self.bgm_stop
+    self.bgs_stop
+    self.se_stop
+    @fmod.dispose
+  end
+  
+  def self.sync_vol
+    [@fmod_bgm, @fmod_bgs].each do |sound_file|
+      if sound_file
+        sound_file.base_volume = $syseting[:bgm_value]
+        FMod.set_volume(sound_file, sound_file.volume)
+      end
+    end
+  end
+end
