329cd3c95f07a8843a6eac15d759e273557f9e89
fux4 <fux4@163.com>
2012-08-13 08:59:16 +0800

    push

--- Game/Data/Scripts/source/CToyHandlers.rb
+++ Game/Data/Scripts/source/CToyHandlers.rb
@@ -3,8 +3,10 @@
     $syseting[:screen_size]=!$syseting[:screen_size]
     $syseting[:screen_size] ? Graphics.resize_screen(800,600) : Graphics.resize_screen(640,480)
     $syseting[:screen_size] ? RGSSX.resize_window(800,600) : RGSSX.resize_window(640,480)
-    SceneManager.scene.spriteset.adapt_screen
-    SceneManager.scene.adapt_screen
+    if SceneManager.scene.is_a?(Scene_Map)
+      SceneManager.scene.spriteset.adapt_screen
+      SceneManager.scene.adapt_screen
+    end
   end
   def self.on_f1
     msgbox "you pressed f1"
